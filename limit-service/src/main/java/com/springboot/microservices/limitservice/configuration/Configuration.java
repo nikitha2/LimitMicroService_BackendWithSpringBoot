f79c35a4bc90b9445f5bec0639bfb0d363b6073a
nikitha2 <gullapalli.nikitha@gmail.com>
2023-05-14 15:59:54 -0700

    credentials

--- limit-service/src/main/java/com/springboot/microservices/limitservice/configuration/Configuration.java
+++ limit-service/src/main/java/com/springboot/microservices/limitservice/configuration/Configuration.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Component;
 public class Configuration {
 	private int minimum;
 	private int maximum;
-
+	private String username;
+	private String password;
 
 	public int getMinimum() {
 		return minimum;
@@ -24,5 +25,18 @@ public class Configuration {
 
 	public void setMaximum(int maximum) {
 		this.maximum = maximum;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
